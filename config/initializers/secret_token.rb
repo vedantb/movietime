806af787d1a6fe125afda550c1dc85bd1ac92600
vedantb <vedantbhatt@gmail.com>
2012-09-18 01:18:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movietime::Application.config.secret_token = 'bd9f090524fe8507fdf762af9a132f7a5f2a78a9e9b36950fc51a14d337c266988c77b3728d45f2d7b1f0eb89c7fb6ef392936358a47d92b7707bcd46dbade16'
